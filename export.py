0d7b0bc033f051350ef2f2fb5520be33d8609546
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 12:32:59 +0530

    impex

---
+++ export.py
@@ -0,0 +1,6 @@
+def greet(name):
+    return " Hello !", name
+
+
+def num(num):
+    return num*2
---
+++ import.py
@@ -0,0 +1,9 @@
+import export
+
+name = input("Enter your name: ")
+greeting = export.greet(name)
+print(greeting)
+
+num = int(input("Enter a number: "))
+result = export.num(num)
+print("Square:", result)
