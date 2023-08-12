3c7292064eb25cf9ab219901fef8d94e742ff7d9
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 11:34:17 +0530

    Basics

--- First.py
+++ First.py
@@ -1,1 +1,11 @@
 print("\n< Hello Humans, \v This is Me Python !! />")
+print("=========================================\n")
+
+# input a which is a number
+a = int(input("Enter A Number : "))
+
+# input b which is a floating number
+b = float(input("Enter B Float : "))
+
+# input a which is a string
+c = input("Enter String : ")
