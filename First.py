7480fc1e101f6fe2ffee056f9e628bd5d2eb3daa
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 11:49:54 +0530

    Basics

--- First.py
+++ First.py
@@ -9,3 +9,12 @@ b = float(input("Enter B Float : "))
 
 # input a which is a string
 c = input("Enter String : ")
+print("\n=========================================\n")
+
+# special format specifiers
+print(f"Values are: \nA: ${a}\nB: ${b}\nC: ${c}")
+print("\n - New line\n")
+print("\t - Tab Space\n")
+print("\v - Vertical Tab Space\n")
+print("\' - Single and \" Double Quote\n")
+
