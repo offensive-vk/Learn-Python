12f0bd3ebc88cfedb54652813f5b3ad4c98053ff
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 12:09:36 +0530

    Basics

---
+++ Pattern.py
@@ -0,0 +1,14 @@
+# Star Pattern
+rows = int(input("Enter the number of rows: "))
+for i in range(1, rows + 1):
+    print("*" * i)
+
+
+# Number Pattern
+rows = int(input("Enter the number of rows: "))
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
