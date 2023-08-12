81b6acbf8c9f2bb9999273dc6dc398d04e010bbe
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 12:22:35 +0530

    Basics

--- Pattern.py
+++ Pattern.py
@@ -11,4 +11,18 @@ for i in range(1, rows + 1):
         print(j, end=" ")
     print()
 
+# Diamond pattern
+size = int(input("Enter the size of the diamond: "))
+for i in range(size):
+    print(" " * (size - i - 1) + "*" * (2 * i + 1))
+for i in range(size - 2, -1, -1):
+    print(" " * (size - i - 1) + "*" * (2 * i + 1))
 
+# Number Pyramid
+rows = int(input("Enter the number of rows: "))
+num = 1
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(num, end=" ")
+        num += 1
+    print()
