73cf0b3a50cd34659e8f78d32f35c34f1d9d0910
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-13 11:32:43 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,23 @@
+# For Loop
+print("For Loop:")
+for i in range(1, 6):
+    print(i, end=" ")
+print("\n")
+
+# While Loop
+print("While Loop:")
+count = 1
+while count <= 5:
+    print(count, end=" ")
+    count += 1
+print("\n")
+
+# Do-While Loop (Simulated)
+print("Do-While Loop (Simulated):")
+count = 1
+while True:
+    print(count, end=" ")
+    count += 1
+    if count > 5:
+        break
+print("\n")
