6baa939557041f9961fee235dcd584baa24920ab
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-13 11:22:09 +0530

    types

---
+++ datatypes.py
@@ -0,0 +1,42 @@
+# Numeric
+int_num = 42
+float_num = 3.14
+complex_num = 2 + 3j
+
+# Sequence
+list_example = [1, 2, 3, 4, 5]
+tuple_example = (6, 7, 8, 9, 10)
+range_example = range(5)
+
+# Mapping (same as object)
+dict_example = {'name': 'John', 'age': 25, 'city': 'New York'}
+
+# Set
+set_example = {1, 2, 3, 4, 5}
+frozenset_example = frozenset({6, 7, 8})
+
+# Bool
+bool_true = True
+bool_false = False
+
+# Printing examples
+print("Numeric examples:")
+print("int:", int_num, type(int_num))
+print("float:", float_num, type(float_num))
+print("complex:", complex_num, type(complex_num))
+
+print("\nSequence examples:")
+print("list:", list_example, type(list_example))
+print("tuple:", tuple_example, type(tuple_example))
+print("range:", range_example, type(range_example))
+
+print("\nMapping example:")
+print("dict:", dict_example, type(dict_example))
+
+print("\nSet examples:")
+print("set:", set_example, type(set_example))
+print("frozenset:", frozenset_example, type(frozenset_example))
+
+print("\nBool examples:")
+print("bool (True):", bool_true, type(bool_true))
+print("bool (False):", bool_false, type(bool_false))
