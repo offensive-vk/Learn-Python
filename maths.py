a667ebb09ec5ee3cdffb818844b07af3ec8ec7fa
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-14 09:30:15 +0530

    maths

---
+++ maths.py
@@ -0,0 +1,16 @@
+print("\n:: Arithmetic Operations :: \n")
+add = 10 + 30  # => 40
+sub = 40 - 10  # => 30
+multiply = 50 * 5  # => 250
+divide = 16 / 4  # => 4.0 (Float Division)
+floor_division = 16 // 4  # => 4 (Integer Division)
+mod = 25 % 2  # => 1
+power = 5 ** 3  # => 125
+
+counter = 0
+counter += 10  # => 10
+counter = 0
+counter = counter + 10  # => 10
+
+message = "Part 1."
+message += "Part 2."
