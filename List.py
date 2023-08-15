068abc5552f8bcb2766a8fbe79afcc147a7eabad
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-15 15:21:11 +0530

    lists

---
+++ List.py
@@ -0,0 +1,44 @@
+# Lists in Python
+# Create a list
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+# Append an item to the end of the list
+# fruits.append('fig')
+
+# Insert an item at a specific position
+fruits.insert(2, 'grape')
+
+# Remove an item from the list
+fruits.remove('cherry')
+
+# Remove an item at a specific index
+removed_item = fruits.pop(1)
+
+# Get the index of an item
+index_banana = fruits.index('banana')
+
+# Count the occurrences of an item
+count_apple = fruits.count('apple')
+
+# Sort the list in ascending order
+fruits.sort()
+
+# Reverse the list
+fruits.reverse()
+
+# Create a copy of the list
+fruits_copy = fruits.copy()
+
+# Extend the list with another list
+more_fruits = ['kiwi', 'lemon', 'mango']
+fruits.extend(more_fruits)
+
+# Clear all items from the list
+fruits.clear()
+
+# Print the results
+print("Original list:", fruits_copy)
+print("Appended list:", fruits)
+print("Removed item:", removed_item)
+print("Index of 'banana':", index_banana)
+print("Count of 'apple':", count_apple)
