0c9eb4eb3548c747742d475a39cfc2640bf38ff5
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-15 15:18:00 +0530

    lists

--- lists.py
+++ lists.py
@@ -12,3 +12,10 @@ print(names)
 names.reverse()
 print(names)
 
+""" Questions
+// Write a program to greet a user by getting name as input 
+// Write a program to take height and width as input and show the area of triangle
+// Write a program to take a number as input and show its table
+// Write a program to create an list of numbers from 1 to 15 and show the result by multiplying the length of list by 15.
+// Write a program to show the sum of the numbers in a list. """
+
