dbf96719439c4758f05665e91726692d8a5dc3c8
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-27 18:29:52 +0530

    More

--- dictionaries.py
+++ dictionaries.py
@@ -4,44 +4,55 @@ student = {
     'name': 'Rupesh Don',
     'age': 20,
     'major': 'Computer Science',
-    'gpa': 3.8,
+    'gpa': 2.8,
     'isPass': True
 }
+print("Old Dictionary: ",student)
 
 # Access values in a dictionary
 student_name = student['name']
 student_age = student.get('age')
+# print(student_name," | " ,student_age)
 
-# Modify values in a dictionary
+# # Modify values in a dictionary
 student['gpa'] = 3.9
+student['major'] = "Commerce"
+print(student)
+#
+# # Add a new key-value pair
+student['favorites'] = ["anime", "manga", "18+"]
+print(student)
 
-# Add a new key-value pair
-student['email'] = 'john.doe@example.com'
-
-# Remove a key-value pair
+# # Remove a key-value pair
 removed_major = student.pop('major')
-
-# Check if a key exists in the dictionary
-has_major = 'major' in student
-
-# Get a list of keys and values
+print(removed_major)
+print(student)
+#
+# # Check if a key exists in the dictionary
+has_major = 'manish' in student
+print(has_major)
+#
+# # Get a list of keys and values
 keys = student.keys()
 values = student.values()
-
-# Get a list of key-value pairs
+print(keys)
+print(values)
+#
+# # Get a list of key-value pairs
 items = student.items()
-
-# Create a copy of the dictionary
+print(items)
+#
+# # Create a copy of the dictionary
 student_copy = student.copy()
-
+#
 # Clear all items from the dictionary
 student.clear()
 
-# Print the results
+# # Print the results
 print("Original dictionary:", student_copy)
 print("Student name:", student_name)
 print("Student age:", student_age)
-print("Updated GPA:", student['gpa'])
+# print("Updated GPA:", student['gpa'])
 print("Removed major:", removed_major)
 print("Has 'major' key:", has_major)
 print("Keys:", keys)
