196a6b5d3d6ad0aacd200303f3eaaed4e8cec244
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-27 18:33:45 +0530

    py

---
+++ Questions.py
@@ -0,0 +1,9 @@
+# dated Aug,25, 2023 ~ Vedansh
+1. write a program that takes names as input and store them in a list
+2. write a program that stores student information in a dictionary , atleast 5 details and print them without {}.
+3. write a program that has a list of length 8 and you should remove that last value and print a message with it.
+4. write a program that list that contains numbers from 99 to 50 and converts into ascending order.
+5. write a program that has one list of 5 fruits names and you should create 5 different copies of it and each list should have 1 different item and should not match.
+
+bonus: write a program that should have 5 different key value pairs in a dictionary and you can perform insertion and deletion operations and you should print the dictionary after every operation yo do or not.
+but the final result should be an {} empty dictionary. <CAUTION AHEAD>.
