0244f3420c60f69edf9b0e5332165f4a843bdfaa
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-27 18:33:59 +0530

    Create EvenOdd.py

---
+++ EvenOdd.py
@@ -0,0 +1,12 @@
+start = int(input("Enter Starting Number :"))
+end = int(input("Enter Ending Number :"))
+
+print("\t\t :: Even Numbers ::\n")
+for num in range(start, end + 1):
+    if num % 2 == 0:
+        print("[",num,"]")
+
+print("\t\t :: Odd Numbers ::\n")
+for num in range(start, end + 1):
+    if num % 2 != 0:
+        print("[",num,"]")
