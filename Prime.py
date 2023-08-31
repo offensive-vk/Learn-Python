6caf80c7fe77460cea753ac37668e4b1bef17260
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 14:43:55 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,4 +1,4 @@
-for i in range(2, 50+1):
+for i in range(2, 50 + 1):
     hai_Prime = True
     for j in range(2, i):
         if i % j == 0:
@@ -8,4 +8,3 @@ for i in range(2, 50+1):
     if hai_Prime:
         print(i)
 
-print("\t -> YOU HACKED NASA !")
