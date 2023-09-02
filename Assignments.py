480983a6704cacad2bb08bde62fdc733d09607a5
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-09-02 18:40:12 +0530

    Create Assignments.py

---
+++ Assignments.py
@@ -0,0 +1,52 @@
+# Assignments as of First Class <Date>
+
+1. Write a program to greet a user by getting name as input 
+2. Write a program to take height and width as input and show the area of triangle
+3. Write a program to take a number as input and show its table
+4. Write a program to create an list of numbers from 1 to 15 and show the result by multiplying the length of list by 15.
+5. Write a program to show the sum of the numbers in a list
+
+# dated Aug,25, 2023 ~ Vedansh
+1. write a program that takes 5 names as input and store them in a list
+2. write a program that stores student information in a dictionary , atleast 5 details and print them without {}.
+3. write a program that has a list of length 8 and you should remove that last value and print a message with it.
+4. write a program that list that contains numbers from 99 to 50 and converts into ascending order.
+5. write a program that has one list of 5 fruits names and you should create 5 different copies of it and each list should have 1 different item and should not match.
+
+bonus: write a program that should have 5 different key value pairs in a dictionary and you can perform insertion and deletion operations and you should print the dictionary after every operation yo do or not.
+but the final result should be an {} empty dictionary. <CAUTION AHEAD>.
+
+# Next Assignments to be submitted 
+6. Write a program to take age and name as input display a message <Authorized Personnel !> if the age is greater than and equal to 25 and the name is "Administrator" else display : <you are not eligible to access>
+7. Design a console calculator to perform 4 different operations:  + - * / respectively.
+8. Write a program and do the following :
+    take first name as input
+    take last name as input 
+    display the finalname in  following format:
+        "Your Full Name: <Rahul Kumar>"
+        rahul is firstname and kumar is lastname with a space in between them.
+
+9. Write a program to display remarks according to the entered marks
+
+Take 5 subject marks as input and display the total marks
+Remarking system should be calculated on total marks. 
+   less than 250 and equal to 250 = poor 
+    300 OR greater = Good
+    350 OR greater = Very Good
+    400 OR greater = Excellent
+    450 OR greater = Keep it up<Excellent>
+    490 OR less then 500 = Outstanding
+
+10. Design a menu driven program that shows 4 options : and there work is written
+1. Prime Numbers: take starting and stopping point , then show result
+2. Even Numbers: take starting and stopping point , then show result
+3 Odd Numbers: take starting and stopping point , then show result
+4 Exit : print a message = thankyou , goodbye
+
+[ Hint: use if else , loops, strings . etc ]
+
+bonus: write a program that takes 5 things input from use with respect to there datatypes:
+-> Name, Age, Salary, Department, Section
+and store that data in dictionary and then prompt the user to display the data or not , if yes then display else say goodbye;
+
+=================================================================
