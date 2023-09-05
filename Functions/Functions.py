01065a2402b9083d9573ce3c57cfcc71f882502b
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-09-05 12:56:19 +0530

    Create Functions.py

---
+++ Functions/Functions.py
@@ -0,0 +1,8 @@
+# Assignments for function <basic>
+1. Write a python program that takes name as function parameter and display it.
+2. write a program that takes base and power as function parameter and display the exponent of the base.(base**power)
+3. write a program that takes 3 names as input and store them in a list using function
+4. write a program that takes shows good morning 10 times using function without using loops
+5. write a program that username as input and displays it in  email format. (name+@msn.net) using functions
+
+bonus3: write a program that takes a name as parameter which is optional and displays it multiple times (multiply it by length of parameter).
