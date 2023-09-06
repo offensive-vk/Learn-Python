6c96bd2aef49bda61248ba89fb70e7d61967d191
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-06 19:40:31 +0530

    Issues

--- Advanced/datetime.py
+++ Advanced/datetime.py
@@ -3,7 +3,7 @@ import datetime
 current_date = datetime.date.today()
 print("Current date:", current_date)
 
-current_time = datetime.datetime.now().time()
+current_time = Advanced.datetime.now().time()
 print("Current time:", current_time)
 
 birth_date = datetime.date(2000, 5, 15)
