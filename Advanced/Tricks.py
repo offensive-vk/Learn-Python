c528c100428f1b8a81292f4502ac5367c2304dd9
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-15 09:53:16 +0530

    Create Tricks.py

---
+++ Advanced/Tricks.py
@@ -0,0 +1,10 @@
+# Calculate the Sum of Numbers in a List:
+numbers = [2, 4, 6, 8, 10]
+sum_numbers = sum(numbers)
+print("Sum of numbers:", sum_numbers)
+
+# Find the Maximum Number in a List:
+numbers = [2, 4, 6, 8, 10]
+max_number = max(numbers)
+print("Maximum number:", max_number)
+
