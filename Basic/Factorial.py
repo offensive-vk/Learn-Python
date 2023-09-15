c43f19717000152d28494dd5f27e4390ef4b3f48
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-15 09:49:03 +0530

    F(X)

---
+++ Basic/Factorial.py
@@ -0,0 +1,10 @@
+# Factorial using recursion.
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+result = factorial(num)
+print("Factorial:", result)
