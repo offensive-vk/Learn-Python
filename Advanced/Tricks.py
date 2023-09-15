287051413472f18b9617146977aa3d21c6f24f24
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-15 09:54:50 +0530

    ~~

--- Advanced/Tricks.py
+++ Advanced/Tricks.py
@@ -8,3 +8,13 @@ numbers = [2, 4, 6, 8, 10]
 max_number = max(numbers)
 print("Maximum number:", max_number)
 
+# Calculate the Average of Numbers in a List
+numbers = [2, 4, 6, 8, 10]
+average = sum(numbers) / len(numbers)
+print("Average of numbers:", average)
+
+# Filter Odd Numbers in a List:
+numbers = [2, 4, 6, 8, 10]
+odd_numbers = [num for num in numbers if num % 2 != 0]
+print("Odd numbers:", odd_numbers)
+
