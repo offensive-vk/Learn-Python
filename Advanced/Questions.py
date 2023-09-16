7d806234959c96eaa482f7c650f272099da2dccc
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-16 12:07:06 +0530

    ez

---
+++ Advanced/Questions.py
@@ -0,0 +1,7 @@
+# OOP Questions ~ Classes, Objects, Maths, Basic
+# 1. Write a python program that calculates area of rectangle using classes.
+# 2. Write a python program that displays good morning to user 5 times using loops and classes
+# 3. Write a python program that takes name and age as input and display them using class and objects.
+# 4. Write a python program that has 5 methods in a class and show inheritance in it.
+# 5. Write a python program that takes name, age, section, address and pin as function parameter and
+# you should create a seperate function for displaying all the entered data in format using classes only.
