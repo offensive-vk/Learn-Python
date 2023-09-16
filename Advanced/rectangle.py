91018de335ae6bc884cd2e106d7860ead4581609
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-16 15:41:57 +0530

    ./

--- Advanced/rectangle.py
+++ Advanced/rectangle.py
@@ -9,5 +9,11 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.width)
 
+# Create rectangle objects and calculate area and perimeter
+rectangle1 = Rectangle(5, 10)
+print("Rectangle 1 Area:", rectangle1.area())
+print("Rectangle 1 Perimeter:", rectangle1.perimeter())
 
-
+rectangle2 = Rectangle(8, 15)
+print("Rectangle 2 Area:", rectangle2.area())
+print("Rectangle 2 Perimeter:", rectangle2.perimeter())
