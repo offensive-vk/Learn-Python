89d5c5fed913d2450386a2ef12add56c7c7ccfa1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-16 15:41:30 +0530

    ~

---
+++ Advanced/rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+
