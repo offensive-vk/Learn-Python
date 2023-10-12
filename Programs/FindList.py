5255c0c07b1abd7fa9245b2d8ae46c70f0bda455
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-12 08:05:41 +0530

    ...

---
+++ Programs/FindList.py
@@ -0,0 +1,9 @@
+def find(lst):
+    smallest = min(lst)
+    largest = max(lst)
+    return smallest, largest
+
+numbers = [5, 2, 9, 1, 7]
+smallest_num, largest_num = find(numbers)
+print("Smallest number:", smallest_num)
+print("Largest number:", largest_num)
