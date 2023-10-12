82fd9089ac43be02cf6bd53a8423ca9049d11b4c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-12 08:04:26 +0530

    ...

--- Programs/Interchange.py
+++ Programs/Interchange.py
@@ -6,3 +6,11 @@ def interchange(lst):
 
 l = [33, 56, 89 , 11, 99]
 print(interchange(l))
+
+# Swap in list
+def swap_elements(lst, index1, index2):
+    lst[index1], lst[index2] = lst[index2], lst[index1]
+
+l = [33, 56, 89, 11, 99]
+swap_elements(l, 0, -1)
+print(l)
