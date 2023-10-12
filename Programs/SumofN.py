7e6c076445b1689268a96c9c6b6cb0346c6feeb9
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-12 07:55:28 +0530

    sum

---
+++ Programs/SumofN.py
@@ -0,0 +1,8 @@
+# Get the value of n from the user
+n = int(input("Enter a number: "))
+
+# Calculate the sum of all numbers from 1 to n
+sum_of_numbers = (n * (n + 1)) // 2
+
+# Print the sum
+print("Sum of numbers from 1 to", n, "is", sum_of_numbers)
