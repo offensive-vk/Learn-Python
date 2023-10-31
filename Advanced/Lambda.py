67aea4a5ff374893e55d0448096820a97c5d2cdd
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-31 16:34:04 +0530

    Update Lambda.py

--- Advanced/Lambda.py
+++ Advanced/Lambda.py
@@ -3,7 +3,15 @@ A lambda function in Python is a small, anonymous function defined using the lam
 Lambda functions are often used for short, simple operations.
 """
 
+# Sum
 add = lambda x, y: x + y
 
 result = add(3, 5)
 print("The sum is:", result)
+
+
+# Square
+square = lambda x: x**2
+
+result = square(4)
+print("The square is:", result)
