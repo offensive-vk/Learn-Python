1d4e139aebec384b4a9a131be52e0958dac97ccf
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-13 14:23:31 +0530

    Update LinkedList.py

--- Advanced/LinkedList.py
+++ Advanced/LinkedList.py
@@ -3,3 +3,53 @@ class Node:
         self.data = data
         self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def delete_node(self, key):
+        current = self.head
+
+        # If the key is in the head
+        if current and current.data == key:
+            self.head = current.next
+            current = None
+            return
+
+        # Search for the key to be deleted
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+
+        # If key is not present
+        if not current:
+            print(f"{key} not found in the list.")
+            return
+
+        # Unlink the node
+        prev.next = current.next
+        current = None
+
