cdbf4569a9fa9c6f53efc68dca73b0658b7909bc
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-13 14:24:06 +0530

    Update LinkedList.py

--- Advanced/LinkedList.py
+++ Advanced/LinkedList.py
@@ -53,3 +53,26 @@ class LinkedList:
         prev.next = current.next
         current = None
 
+# Example usage
+if __name__ == "__main__":
+    # Create a linked list
+    linked_list = LinkedList()
+
+    # Insert some elements
+    linked_list.insert_at_end(1)
+    linked_list.insert_at_end(2)
+    linked_list.insert_at_end(3)
+
+    # Display the linked list
+    print("Linked List:")
+    linked_list.display()
+
+    # Insert at the beginning
+    linked_list.insert_at_beginning(0)
+    print("\nLinked List after inserting at the beginning:")
+    linked_list.display()
+
+    # Delete a node
+    linked_list.delete_node(2)
+    print("\nLinked List after deleting node with value 2:")
+    linked_list.display()
