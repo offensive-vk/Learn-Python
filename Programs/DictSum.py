7631cb57aa951d899ea1652721f4997a66bfade6
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-25 21:14:33 +0530

    Programs

--- Programs/DictSum.py
+++ Programs/DictSum.py
@@ -1,1 +1,11 @@
 # Write a python program to find the sum of dictionary values
+def sum_dictionary_values(dictionary):
+    total_sum = 0
+    for value in dictionary.values():
+        total_sum += value
+    return total_sum
+
+# Example usage
+my_dictionary = {"a": 1, "b": 2, "c": 3}
+sum_of_values = sum_dictionary_values(my_dictionary)
+print("The sum of the dictionary values is:", sum_of_values)
