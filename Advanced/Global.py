0d1c0356e97e9904c87339c97122940209d1cf5f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-10 11:32:36 +0530

    **

---
+++ Advanced/Global.py
@@ -0,0 +1,12 @@
+class Global:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.salary = salary
+        self.age = age
+
+    def DisplayData(self, obj) -> str:
+        print(f"Name : {obj.name}")
+        print(f"Age : {obj.age}")
+        print(f"Salary : {obj.salary}")
+
+        return obj
