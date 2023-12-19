b380fd7c5e24ec95bce6fc7707a5e6f9d53d929c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-19 10:38:12 +0530

    Dunders

--- Advanced/Dunders.py
+++ Advanced/Dunders.py
@@ -29,3 +29,47 @@ __enter__(self), __exit__(self, exc_type, exc_value, traceback): Used for contex
 
 __call__(self, ...): Allows an object to be called like a function.
 """
+
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+        print(f"Object initialized with name: {self.name}")
+
+    def __repr__(self):
+        return f"MyClass(name={self.name})"
+
+    def __str__(self):
+        return f"This is an instance of MyClass with name: {self.name}"
+
+    def __add__(self, other):
+        if isinstance(other, MyClass):
+            return MyClass(name=f"{self.name}_{other.name}")
+        else:
+            raise TypeError("Unsupported operand type for +: MyClass and {type(other)}")
+
+    def __len__(self):
+        return len(self.name)
+
+    def __init_subclass__(cls, **kwargs):
+        print(f"Subclass {cls.__name__} is being created.")
+
+# Creating an instance of MyClass
+obj1 = MyClass("Object1")
+
+# Using the __repr__ and __str__ methods
+print("Using __repr__:", repr(obj1))
+print("Using __str__:", str(obj1))
+
+# Using the __add__ method
+obj2 = MyClass("Object2")
+combined_obj = obj1 + obj2
+print("Combined object:", combined_obj)
+
+# Using the __len__ method
+print("Length of obj1:", len(obj1))
+
+# Creating a subclass
+class MySubclass(MyClass):
+    pass
+
+# Output will show that the subclass is being created due to __init_subclass__
