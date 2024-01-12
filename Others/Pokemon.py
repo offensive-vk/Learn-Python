3a7e3f92bbe4f894efef0929c8659d122f0dad78
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-12 14:41:49 +0530

    Pokemon.exe

--- Others/Pokemon.py
+++ Others/Pokemon.py
@@ -1,3 +1,11 @@
 import random
 import time
 
+class Pokemon:
+    def __init__(self, name, level, health, attack, defense):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
