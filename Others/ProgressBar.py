8b140f31778da560e49470a0371f867722f40aa5
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-16 18:21:39 +0530

    Progress

--- Others/ProgressBar.py
+++ Others/ProgressBar.py
@@ -3,3 +3,13 @@ from time import sleep
 
 for i in tqdm(range(100)):
     sleep(0.05)
+
+# Example loop with tqdm progress bar
+for i in tqdm(range(100), desc="Processing"):
+    # Simulating some time-consuming operation
+    sleep(0.1)
+
+# Example loop with a styled tqdm progress bar
+for i in tqdm(range(10), desc="Processing", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]"):
+    # Simulating some time-consuming operation
+    sleep(0.1)
