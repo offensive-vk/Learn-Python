04e256a4908ad0cf3a229a9e64b82ca627858b68
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-16 18:00:29 +0530

    OS

---
+++ Libraries/OS/Basic.py
@@ -0,0 +1,6 @@
+"""
+The os module in Python provides a way of interacting with the operating system,
+allowing you to perform various tasks like file and directory operations.
+"""
+import os
+print(f"Files in current working directory: {os.getcwd()}")
