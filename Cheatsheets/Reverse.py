09c1831bd47e5f51f08e0ebbf82290b7106470dc
Vedansh <superuser.ntsystems@outlook.com>
2024-08-28 15:40:38 +0530

    Add One Lined Reverse.py (#20)
    Create Reverse.py

---
+++ Cheatsheets/Reverse.py
@@ -0,0 +1,8 @@
+# Number reverse
+print(int(str(int(input("Enter a number: ")))[::-1]))
+
+# String + Number reverse
+print(str(input("Enter a string : ")[::-1]))
+
+# More compact can be
+print(str(input()[::-1]))
